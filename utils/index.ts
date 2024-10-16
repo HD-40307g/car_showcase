1fc2d5d570cd7a25f9b7b940053711c2d2b93bcd
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-16 20:31:07 +0200

    Add fetch function from API and render it in page.tsx

--- utils/index.ts
+++ utils/index.ts
@@ -1,24 +1,10 @@
-const url = 'https://cars-by-api-ninjas.p.rapdapi.com/v1/cars?model=corolla';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiyzkRomp15f2vKjsnK4bKzuUzVlza',
-        'X-RapidAPI-Host' : 'cars-by-api-ninjas.p.rapidapi.com'
-    }
-};
-
-try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-} catch (error) {
-    console.log(error)
-}
-
 export async function fetchCars() {
     const headers = {
-        'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiyzkRomp15f2vKjsnK4bKzuUzVlza',
+        'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiyzkRomp15f2vKjsnK4bKzuUzVlza', /*Get Custom API-KEY*/
         'X-RapidAPI-Host' : 'cars-by-api-ninjas.p.rapidapi.com'
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapdapi.com/v1/cars?model=corolla');
+    const response = await fetch('https://cars-by-api-ninjas.p.rapdapi.com/v1/cars', { headers: headers });
+
+    const result = await response.json();
+    return result;
 }

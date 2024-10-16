b4e9c3afe40525e323932fc4d1f04ceb12b6cca6
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-16 20:36:58 +0200

    CarCard.tsx component initial commit

---
+++ components/CarCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+
+const CarCard = () => {
+    return (
+    <div>CarCard</div>
+    )
+}
+
+export default CarCard;

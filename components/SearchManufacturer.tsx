6192c42f90e98bd175d41a0195c02a71bb106c14
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-16 18:26:44 +0200

    Add styling from HeadlessUI

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -15,7 +15,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
     return (
     <div className='search-manufacturer'>
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
             <div className='relative w-full'>
                 <ComboboxButton className='absolute top-15px'>
                     <Image src="/car-logo-svg" width={20} height={20} className='ml-4' alt='Car Logo' />
@@ -24,7 +24,17 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                 <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => setQuery('')}>
                     <Combobox.Options>{filteredManufacturers.map((item) => (
                         <Combobox.Option key={item} className={({active}) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`} value={item}>
-                            {item}
+                            {({ selected, active }) => (
+                                <>
+                                <span className={`block truncate ${selected ? 'fond-medium' : 'font-normal'}`}>
+                                    {item}
+                                </span>
+                                {selected ? (
+                                    <span className={`absolute insert-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'}`}>
+                                    </span>
+                                ) : null} 
+                                </>
+                            )}
                         </Combobox.Option>
                             )
                         )}

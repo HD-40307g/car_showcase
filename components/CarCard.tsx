035094e0773b7666bda75e14effc3a8f7ab74949
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-18 18:05:53 +0200

    Add styling, button and icons to CarCard.tsx

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -13,6 +13,7 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, year, make, model, transmission, drive } = car;
+    const [isOpen, setIsOpen] = useState(false);
     const carRent = calculateCarRent(city_mpg, year);
 
     return (
@@ -28,9 +29,26 @@ const CarCard = ({ car }: CarCardProps) => {
         <div className='relative w-full h-40 my-3 object-contain'>
             <Image src='/hero.png' alt='car model' fill priority className='object-contain'/>
         </div>
-        <div>
-            
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w_full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <img src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
+                    <p className='text-[14px]'>{transmission === 'a' ? 'Automatic' : "Manual"}</p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <img src="/tire.svg" width={20} height={20} alt="tire" />
+                    <p className='text-[14px]'>{drive.toUpperCase()}</p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <img src="/gas.svg" width={20} height={20} alt="gas" />
+                    <p className='text-[14px]'>{city_mpg} MPG</p>
+                </div>
+            </div>
+            <div className='car-card__btn-container'>
+                <CustomButton title='View More' containerStyles='w-full py-[16px] rounded-full bg-primary-blue' textStyles='text-white text-[14px] leading-[17px] font-bold' rightIcon='/right-arrow.swg' handleClick={() => setIsOpen(true)} />
+            </div>
         </div>
+        <CarDetails />
     </div>
     )
 }

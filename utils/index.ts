5a29b6f0f0c479a32d2b833656c917b07ec40c74
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-19 22:35:35 +0200

    Add searchParamss to fetch response in page.tsx

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
     const headers = {
         'X-RapidAPI-Key': 'KJwZZIJSFimshuivMSVGaiyzkRomp15f2vKjsnK4bKzuUzVlza', /*Get Custom API-KEY*/
         'X-RapidAPI-Host' : 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapdapi.com/v1/cars?model=carrera', { headers: headers });
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapdapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, { headers: headers });
 
     const result = await response.json();
     return result;

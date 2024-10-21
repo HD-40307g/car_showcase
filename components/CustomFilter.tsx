32b033b2c578241ba71d9a54f541610e46d24dbb
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-21 15:41:55 +0200

    Add handleUpdateParams function in CustomFilter.tsx component

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -6,12 +6,19 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, ListboxButton, ListboxOption, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({title, options}: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
+  
+  const handleUpdateParams = (e: { title: string, value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName);
+  }
+
   return (
     <div className='w-fit'>
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox value={selected} onChange={(e) => setSelected(e), handleUpdateParams(e)}>
         <div className='relative w-fit z-10'>
           <ListboxButton className='custom-filter__btn'>
             <span>{selected.title}</span>

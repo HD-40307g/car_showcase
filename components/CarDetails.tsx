b0432e86d83e50980d4dc7551c012a67c20a82bf
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-24 16:58:07 +0200

    Refactor CustomButton.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -22,13 +22,13 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                     </TransitionChild>
                     <div className='fixed inset-0 overflow-y-auto'>
                         <div className='flex min-h-full items-center justify-center p-4 text-center'>
-                            <TransitionChild as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
-                                <DialogPanel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
-                                    <button type='button' className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'>
+                            <TransitionChild as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-300' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
+                                <DialogPanel className='car-details__dialog-panel'>
+                                    <button type='button' className='car-details__close-btn' onClick={closeModal}>
                                         <Image src='/close.svg' alt='close' width={20} height={20} className='object-contain'/>
                                     </button>
                                     <div className='flex-1 flex flex-col gap-3'>
-                                        <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                                        <div className='car-details__main-image'>
                                             <Image src={generateCarImageUrl(car, 'angle')} alt='car model' fill priority className='object-contain' />
                                         </div>
                                         <div className='flex gap-3'>
@@ -48,7 +48,7 @@ const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
                                         <div className='mt-3 flex flex-wrap gap-4'>
                                             {Object.entries(car).map(([key, value]) => (
                                                 <div className='flex justify-between gap-5 w-full text-right' key={key}>
-                                                    <h4 className='text=grey capitalize'>{key.split("_").join(" ")}</h4>
+                                                    <h4 className='text-grey capitalize'>{key.split("_").join(" ")}</h4>
                                                     <p className='text-black-100 font-semibold'>{value}</p>
                                                 </div>
                                             ))}
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React from 'react'
+
 import Image from 'next/image';
 import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisabled }: CustomButtonProps) => {
     return ( 
-        <button disabled={false} type={btnType ||'button'} className={`custom-btn ${containerStyles}`} onClick={() => {}}>
+        <button disabled={isDisabled} type={btnType ||'button'} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
             <span className={`flex-1 ${textStyles}`}>{title}</span>
             {rightIcon && (
                 <div className='relative w-6 h-6'>
-                    <Image src={rightIcon} alt='right icon' fill className='object-contain' />
+                    <Image src={rightIcon} alt='arrow left' fill className='object-contain' />
                 </div>
             )}
         </button>

acc46e3035ec2ce92b005ebe6f498c211e38751d
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-26 19:49:51 +0200

    add aria-disabled for accessibility purposes

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,11 +5,11 @@ import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon, isDisabled }: CustomButtonProps) => {
     return ( 
-        <button disabled={isDisabled} type={btnType ||'button'} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+        <button disabled={isDisabled} type={btnType ||'button'} aria-disabled={isDisabled} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
             <span className={`flex-1 ${textStyles}`}>{title}</span>
             {rightIcon && (
                 <div className='relative w-6 h-6'>
-                    <Image src={rightIcon} alt='arrow left' fill className='object-contain' />
+                    <Image src={rightIcon} alt={`icon for ${title}`} fill className='object-contain' />
                 </div>
             )}
         </button>

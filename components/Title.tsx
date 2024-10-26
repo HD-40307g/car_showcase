a9e5ede43cdc31711367632d0ba48b8bbf8690b1
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-26 20:50:54 +0200

    Refactor handleScroll function in Title.tsx component

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,25 +2,24 @@
 
 import Image from 'next/image';
 import CustomButton from './CustomButton';
+import { useCallback } from 'react';
 
 const Title = () => {
-    const handleScroll = () => {
+    const handleScroll = useCallback(() => {
         const nextSection = document.getElementById("discover");
         if (nextSection) {
             nextSection.scrollIntoView({ behavior: "smooth" });
+        } else {
+            console.warn("Element with ID 'discover' not found for scrolling.");
         }
-    };
+    }, []);
 
         return (
-        <div className='hero'>
+        <header className='hero'>
             <div className='flex-1 pt-36 padding-x'>
                 <h1 className='hero__title'>Find, book, or rent a car - quick and easy!</h1>
                 <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
-                <CustomButton 
-                    title="Explore Cars" 
-                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
-                    handleClick={handleScroll}
-                />
+                <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
             </div>
             <div className="hero__image-container">
                 <div className='hero__image'>
@@ -28,7 +27,7 @@ const Title = () => {
                 </div>
                 <div className='hero__image-overlay'/>
             </div>
-        </div>
+        </header>
     );
 };
 

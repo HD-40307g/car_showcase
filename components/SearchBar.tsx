c77323a1c2980bbdd412f0036a40b78de8915f58
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-26 20:14:12 +0200

    Add lable element and dynamic error message

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,22 +6,25 @@ import Image from 'next/image';
 import { SearchBarProps } from "@/types";
 
 const SearchButton = ({ otherClasses }: {otherClasses: string}) => (
-    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
-        <Image src='/magnifying-glass.svg' alt='magnifying glass' width={40} height={40} className='object-contain' />
+    <button type='submit' className={`-ml-3 z-10 ${otherClasses}`} aria-label="Search">
+        <Image src='/magnifying-glass.svg' alt='Search magnifying glass' width={40} height={40} className='object-contain' />
     </button>
 );
 
 const SearchBar = ({ setManufacturer, setModel }: SearchBarProps) => {
     const [searchManufacturer, setSearchManufacturer] = useState('');
     const [searchModel, setSearchModel] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         if(searchManufacturer.trim() === '' && searchModel.trim() === '') {
-            return alert('Please fill in the search bar')
+            setErrorMessage('Please fill in at least one search field');
+            return;
         }
 
+        setErrorMessage('');
         setModel(searchModel);
         setManufacturer(searchManufacturer);
     };
@@ -33,10 +36,12 @@ const SearchBar = ({ setManufacturer, setModel }: SearchBarProps) => {
                 <SearchButton otherClasses='sm:hidden' />
             </div>
             <div className='searchbar__item'>
+                <label htmlFor="model" className="sr-only">Car Model</label>
                 <Image src='/public/model-icon.png' width={25} height={25} className='object-contain' alt='car model' />
                 <input type='text' name='model' value={searchModel} onChange={(e) => setSearchModel(e.target.value)} placeholder='Arteon' className='searchbar__input'/>
                 <SearchButton otherClasses='sm:hidden' />
             </div>
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
             <SearchButton otherClasses='max-sm:hidden' />
         </form>
     )
